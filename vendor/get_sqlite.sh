486c39ef2c9eb4946295e2b1ed4049def33615fd
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-01-16 13:04:41 -0800

    vendor dir, submodules

---
+++ vendor/get_sqlite.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://www.sqlite.org/2022/sqlite-autoconf-3400100.tar.gz
+tar -xvzf sqlite-autoconf-3400100.tar.gz
+rm sqlite-autoconf-3400100.tar.gz
+mv sqlite-autoconf-3400100/ sqlite
