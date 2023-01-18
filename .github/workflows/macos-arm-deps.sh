dd7a47bafcd8db87e9f186df48455509fe98d123
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-01-17 16:16:33 -0800

    just skip macos arm for now

--- .github/workflows/macos-arm-deps.sh
+++ .github/workflows/macos-arm-deps.sh
@@ -4,7 +4,5 @@ set -euxo pipefail
 #setopt sh_word_split
 mkdir arm-homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew
 response=$(./arm-homebrew/bin/brew fetch --force --bottle-tag=arm64_big_sur llvm | grep "Downloaded to")
-parsed=($response)
-echo $response
-echo $parsed
-./arm-homebrew/bin/brew install $parsed[3]
+location=$(echo $response | awk '{ print $3 }')
+./arm-homebrew/bin/brew install $location
